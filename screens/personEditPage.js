e86edd515c6e711be3cc5d984bbffd24e6448f4a
Chris <36681915+cstevenson93@users.noreply.github.com>
2022-02-19 17:10:48 -0500

    Implemented isActive toggle

--- screens/personEditPage.js
+++ screens/personEditPage.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import { ActivityIndicator, Image,TouchableOpacity, Platform,  Text, View, FlatList, StyleSheet } from 'react-native';
+import React, { Component, useState } from 'react';
+import { ActivityIndicator, Image,TouchableOpacity, Platform,  Text, View, FlatList, StyleSheet, TextInput, Picker } from 'react-native';
+import { Switch } from 'react-native-switch';
 import Stylesheet from './Stylesheet';
 
 export default class personEditPage extends Component {  
@@ -18,7 +19,9 @@ componentWillMount= async() =>{
   var person = this.props.navigation.state.params.person;
 
   this.setState({
-    personInfo: person
+    personInfo: person,
+    personActive: person.isActive,
+    gender: 'male'
   })
 }
 
@@ -29,7 +32,11 @@ componentDidMount= async() =>{
     
 }
 
-
+changeActiveState = async(val) =>{
+  this.setState({
+  personActive: val,
+  })
+}
 
 
 
@@ -43,12 +50,54 @@ componentDidMount= async() =>{
             );
           }
 
+          const [selectedValue, setSelectedValue] = this.state.gender;
           
         
         return (  
             
         <View style={styles.container}>
-      <Text>{this.state.personInfo.name}</Text>
+       
+        <TextInput
+          style={styles.input}
+          //onChangeText={onChangeText}
+          placeholder="Name"
+        />
+
+        <Switch
+        value={this.state.personActive}
+        onValueChange={(val) => this.changeActiveState(val)}
+        disabled={false}
+        activeText={'isActive'}
+        inActiveText={'Disabled'}
+        backgroundActive={'green'}
+        backgroundInactive={'gray'}
+        circleActiveColor={'#30a566'}
+        circleInActiveColor={'#000000'}
+        width={100}/>
+      
+        <TextInput
+          style={styles.input}
+          //onChangeText={onChangeText}
+          placeholder="Age"
+          keyboardType='number-pad'
+        />
+
+        <TextInput
+          style={styles.input}
+          //onChangeText={onChangeText}
+          placeholder="About"
+        />
+
+        <Picker
+          selectedValue={selectedValue}
+          style={{ height: 45, width: 180 }}
+          //onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+          placeholder="Pick a Gender"
+        >
+          <Picker.Item label="Male" value="male" />
+          <Picker.Item label="Female" value="female" />
+        </Picker>
+      
       </View>
             
         );  
@@ -64,5 +113,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
